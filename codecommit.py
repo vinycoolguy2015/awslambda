01836e29627b6f6675c61400dba6f6afd192db65
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2020-08-04 18:20:13 +0800

    Update codecommit.py

--- codecommit.py
+++ codecommit.py
@@ -4,9 +4,12 @@ def lambda_handler(event, context):
     repo='test'
     normal_sns='arn:aws:sns:us-east-1:953898941182:normal'
     emergency_sns='arn:aws:sns:us-east-1:953898941182:emergency'
+    
     codecommit_client = boto3.client('codecommit')
     sns_client = boto3.client('sns')
+    
     commitresponse = codecommit_client.get_differences(repositoryName=repo,afterCommitSpecifier=event['Records'][0]['codecommit']['references'][0]['commit'])
+    
     for differences in commitresponse['differences']:
         if differences['afterBlob']['path'] == 'normal.txt':
             data = codecommit_client.get_file(repositoryName=repo,filePath=differences['afterBlob']['path'])
