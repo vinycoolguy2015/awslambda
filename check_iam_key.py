03c30202b3da47b4ceb0486a23db848516aadd8c
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2020-08-29 19:49:11 +0800

    Create check_iam_key.py

---
+++ check_iam_key.py
@@ -0,0 +1,28 @@
+import os,re
+import boto3
+from botocore.exceptions import ClientError
+
+account1_client = boto3.client(	'iam',
+			aws_access_key_id="",
+			aws_secret_access_key=""
+		)
+account2_client = boto3.client(   'iam',
+                        aws_access_key_id="",
+                        aws_secret_access_key=""
+                ) 
+
+
+access_keys=[]
+
+for access_key in access_keys:
+    try:
+        response = account1_client.get_access_key_last_used(AccessKeyId=access_key)
+    except ClientError as e:
+        try:
+            response = account2_client.get_access_key_last_used(AccessKeyId=access_key)
+        except ClientError as e:
+            print("Access key %s not found in the given AWS accounts." % (access_key))
+        else:
+            print( "Access_key %s found in account2"% (access_key))
+    else:
+        print( "Access_key %s found in account1"% (access_key))
