5d71ccb4d74408543370eac5f3965473a2fd2679
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2020-09-09 02:02:08 +0800

    Update imdb_data.py

--- imdb_data.py
+++ imdb_data.py
@@ -7,9 +7,10 @@ ua = UserAgent()
 header = {'user-agent':ua.chrome}
 
 genre=raw_input("Please select one of the given genre:action,animation,adventure,biography,comedy,crime,documentary,drama,family,fantasy,history,horror,music,mystery,romance,sci_fi,sport,thriller,war ")
-
 rating=float(raw_input("Please select the minimum rating of the movie "))
 year=int(raw_input("Please select the minimum release year of the movie "))
+
+print("Searching Movies for you \n\n")
 
 BASE_URL='http://www.imdb.com/search/title?genres='
 
