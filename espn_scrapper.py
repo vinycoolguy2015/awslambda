de351ff2408fb0f5fee5927fd23f8f41db6ce5d7
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2020-09-15 09:52:23 +0800

    Update espn_scrapper.py

--- espn_scrapper.py
+++ espn_scrapper.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from bs4 import BeautifulSoup
 import re
 
 
