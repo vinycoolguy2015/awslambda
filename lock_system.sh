da96694133f6206423c2b01b67114e0f6fa01940
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2020-09-17 21:46:28 +0800

    Create lock_system.sh

---
+++ lock_system.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo /usr/bin/osascript -e 'tell application "System Events" to do shell script "/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend"'
