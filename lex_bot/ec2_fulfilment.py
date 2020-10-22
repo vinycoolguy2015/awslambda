35045761a22d1b02987e44d4bc0620a891bdd21e
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2020-10-23 01:34:32 +0800

    Create ec2_fulfilment.py

---
+++ lex_bot/ec2_fulfilment.py
@@ -0,0 +1,21 @@
+import boto3
+
+def fullfilment_handler(event, context):
+    client = boto3.client('ec2',region_name=event['sessionAttributes']['region'])
+    volumeId = event['currentIntent']['slots']['instanceDetails']
+    
+
+    response = {
+        "dialogAction": {
+            "type": "Close",
+            "fulfillmentState": "Fulfilled",
+            "message": {
+                "contentType": "PlainText",
+                "content": (
+                    "Thanks, I have started creating snapshot for {0}."
+                ).format(volumeId)
+            }
+        }
+    }
+    client.create_snapshot(VolumeId=volumeId,Description=volumeId+"_Snap")
+    return response
