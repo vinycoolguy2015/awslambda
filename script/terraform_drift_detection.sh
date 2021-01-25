5d8cab3baa1d2ebb1d19c15882014de2148d0361
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-01-25 17:17:24 +0800

    Update terraform_drift_detection.sh

--- script/terraform_drift_detection.sh
+++ script/terraform_drift_detection.sh
@@ -4,6 +4,6 @@ case $ec in
 0) echo "No Changes Found": exit 0;;
 1) printf '%s/n' "Command exited with non-zero";exit 1;;
 2) echo "Changes found";
-   MESSAGE=$(terraform show tf.plan| awk '/#/,EOF { print $0 }');
-   echo "$MESSAGE"
+   MESSAGE=$(terraform show -no-color tf.plan| awk '/#/,EOF { print $0 }');
+   curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" <Slack_Webhook_URL>
 esac
