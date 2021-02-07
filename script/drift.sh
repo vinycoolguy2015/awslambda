1e38c3be0aa15c8481f52103cb531095d8d01f60
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-02-08 00:01:14 +0800

    Update drift.sh

--- script/drift.sh
+++ script/drift.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
+  
+basedir=$(pwd)
+export terraform_provider_platform='darwin_amd64'
+slack_url=''
 
 for dir in $(find environments/qa -type d -maxdepth 2 -mindepth 2); do
-  printf  '\nChecking Directory: %s \n' "$dir"
-  cd -
-  cd $dir
-  terragrunt init -plugin-dir=../../terraform_aws_plugins/plugins/darwin_amd64 
-  terragrunt plan --detailed-exitcode -out=tf.plan 2> /dev/null || ec=$?
-  case $ec in
-  0) echo "No Changes Found": exit 0;;
-  1) printf '%s/n' "Command exited with non-zero.Check issues with terragrunt code";;
-  2) echo "Changes found";
-     MESSAGE=$(terraform show -no-color tf.plan| awk '/#/,EOF { print $0 }');
-     #curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" <Slack_Webhook_URL>
-     echo $MESSAGE
-  esac
+  MESSAGE=''
+  rm -rf /tmp/tf.plan
+  printf  '\nChecking Directory: %s \n' "$dir" >> /tmp/test.txt
+  cd $basedir/$dir
+  terragrunt init -plugin-dir=$basedir/terraform_aws_plugins/plugins/darwin_amd64 #Change it to Linux one
+  terragrunt plan  -out=/tmp/tf.plan
+  if [ $? -ne 0 ];then
+    MESSAGE="Terragrunt plan for $dir exited with non zero exit code.Please check issues with code."
+  fi
+  if terragrunt show -no-color /tmp/tf.plan| grep "#";then
+    MESSAGE="Drift status for $dir\n\n"$(terragrunt show -no-color /tmp/tf.plan| awk '/#/,EOF { print $0 }');
+  fi
+
+  #Send message to Slack if MESSAGE is non-empty
+  [[ ! -z "$MESSAGE" ]] && curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" $slack_url || echo "No changes detected"
 done
-
-
-
-
-
-
