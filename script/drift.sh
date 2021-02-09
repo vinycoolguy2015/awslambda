b7cdf16df1febe72922531c10aabdb63eafac239
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-02-09 20:44:04 +0800

    Update drift.sh

--- script/drift.sh
+++ script/drift.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
-  
+
+#For testing only
+export AWS_ACCESS_KEY_ID=''
+export AWS_SECRET_ACCESS_KEY=''
+
+
+export terraform_provider_platform='linux_amd64'
+slack_url='https://hooks.slack.com/services/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'
+MESSAGE=''
+
 basedir=$(pwd)
-export terraform_provider_platform='darwin_amd64'
-slack_url=''
-
-for dir in $(find environments/qa -type d -maxdepth 2 -mindepth 2); do
-  MESSAGE=''
-  rm -rf /tmp/tf.plan
-  printf  '\nChecking Directory: %s \n' "$dir" >> /tmp/test.txt
-  cd $basedir/$dir
-  terragrunt init -plugin-dir=$basedir/terraform_aws_plugins/plugins/darwin_amd64 #Change it to Linux one
-  terragrunt plan  -out=/tmp/tf.plan
-  if [ $? -ne 0 ];then
-    MESSAGE="Terragrunt plan for $dir exited with non zero exit code.Please check issues with code."
-  fi
-  if terragrunt show -no-color /tmp/tf.plan| grep "#";then
-    MESSAGE="Drift status for $dir\n\n"$(terragrunt show -no-color /tmp/tf.plan| awk '/#/,EOF { print $0 }');
-  fi
-
-  #Send message to Slack if MESSAGE is non-empty
-  [[ ! -z "$MESSAGE" ]] && curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" $slack_url || echo "No changes detected"
+for dir in $(find environments/gds-pdd-bgp.dev -type d -maxdepth 2 -mindepth 2); do
+	cd $basedir/$dir
+	rm -rf /tmp/tf.plan
+	printf  '\nChecking Directory: %s \n' "$dir"
+	terragrunt init
+	terragrunt plan -out=/tmp/tf.plan
+	if [ $? -ne 0 ];then
+    	MESSAGE="Terragrunt plan for $dir exited with non zero exit code.Please check for code or permission related issues by executing terragrunt plan manually."
+	else
+		if terragrunt show -no-color /tmp/tf.plan| grep "#";then
+    		MESSAGE="Drift status for $dir\n\n"$(terragrunt show -no-color /tmp/tf.plan| awk '/#/,EOF { print $0 }');
+		fi
+	fi
+	[[ ! -z "$MESSAGE" ]] && curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" $slack_url || echo "No chnages detected"
 done
+
+#Now execute docker run --rm --name diff_checker -v `pwd`:/apps alpine/terragrunt:0.12.17 /bin/bash -c ' ./terraform_drift_check.sh'
