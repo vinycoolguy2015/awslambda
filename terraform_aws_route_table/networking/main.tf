ed8afd3de96ef2745f9303f6536d9e27065ad90e
Ubuntu <ubuntu@ip-172-31-70-235.ec2.internal>
2021-02-24 02:37:34 +0000

    terraform_aws_route_table

--- terraform_aws_route_table/networking/main.tf
+++ terraform_aws_route_table/networking/main.tf
@@ -25,45 +25,45 @@ resource "aws_vpc" "test_vpc" {
 }
 
 
-resource "aws_subnet" "mtc_private_subnet" {
+resource "aws_subnet" "test_private_subnet" {
   count                   = var.private_sn_count
-  vpc_id                  = aws_vpc.mtc_vpc.id
+  vpc_id                  = aws_vpc.test_vpc.id
   cidr_block              = var.private_cidrs[count.index]
   map_public_ip_on_launch = false
   availability_zone       = random_shuffle.public_az.result[count.index]
 
   tags = {
-    Name = "mtc_private_${count.index + 1}"
+    Name = "test_private_${count.index + 1}"
   }
   lifecycle {
     ignore_changes = [availability_zone]
   }
 }
 
-resource "aws_default_route_table" "mtc_private_rt" {
-  default_route_table_id = aws_vpc.mtc_vpc.default_route_table_id
+resource "aws_default_route_table" "test_private_rt" {
+  default_route_table_id = aws_vpc.test_vpc.default_route_table_id
 
   tags = {
-    Name = "mtc_private"
+    Name = "test_private"
   }
 }
 
 resource "aws_route" "aws_route_gateway" {
-  route_table_id = aws_vpc.mtc_vpc.default_route_table_id
+  route_table_id = aws_vpc.test_vpc.default_route_table_id
   for_each               = var.create_gateway_routes ? var.routes.gateway : {}
   destination_cidr_block = each.key
   gateway_id             = each.value
 }
 
 resource "aws_route" "aws_route_peering" {
-  route_table_id = aws_vpc.mtc_vpc.default_route_table_id
+  route_table_id = aws_vpc.test_vpc.default_route_table_id
   for_each                  = var.create_peering_routes ? var.routes.peering : {}
   destination_cidr_block    = each.key
   vpc_peering_connection_id = each.value
 }
 
 resource "aws_route" "aws_route_nat" {
-  route_table_id = aws_vpc.mtc_vpc.default_route_table_id
+  route_table_id = aws_vpc.test_vpc.default_route_table_id
   for_each               = var.create_nat_routes ? var.routes.nat_gateway : {}
   destination_cidr_block = each.key
   nat_gateway_id         = each.value
