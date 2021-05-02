d50e4f1ed78d4f6cf8c616d7c3bd163e354c3533
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-05-02 16:06:28 +0800

    Update script.sh

--- gcp_cloudrun_apigateway/script.sh
+++ gcp_cloudrun_apigateway/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-project=terraform-312414
+project=terraform-3124
 cloudrun_url=$(gcloud run services describe app --platform managed --project $project --region us-east1 --format 'value(status.url)' | cut -d '/' -f3)
 sed -i "s/CLOUD_RUN_URL/$cloudrun_url/g" ~/gcp_cloudrun_apigateway/api_gateway/spec.yaml
