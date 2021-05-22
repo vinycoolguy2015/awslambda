60b70d5df9b4f21842221a5d0462989cec5fd71f
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-05-23 00:13:13 +0800

    Create install_jenkins.sh

---
+++ ha_jenkins_on_gcp/install_jenkins.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv kubectl /usr/bin && sudo kubectl apply -f jenkins.yaml
