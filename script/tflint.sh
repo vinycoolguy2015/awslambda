bce650681ef71055d4988548eec8c24445ae83bd
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-06-29 10:15:53 +0800

    Update tflint.sh

--- script/tflint.sh
+++ script/tflint.sh
@@ -7,8 +7,8 @@ yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
 
-TFLINT_VERSION=${TFLINT_VERSION:-0.9.3}
-TF_VERSION=${TF_VERSION:-0.12.9}
+TFLINT_VERSION=${TFLINT_VERSION:-0.24.1}
+TF_VERSION=${TF_VERSION:-0.13.6}
 TFLINT_CONFIG=$(cat .tflint.hcl)
 
 docker pull wata727/tflint:${TFLINT_VERSION}
@@ -20,24 +20,20 @@ export exit_status=0
 # check ./modules dir
 for dir in $(find modules ! -path modules -type d); do
   printf  '\nChecking Directory: %s \n' "$dir"
-  if [[ $dir =~ _lower_env_only_pls ]]; then
-    printf  "Skipping check for modules not used in production for the time being... \n\n"
-  else
-    echo  "| TFLINT CHECK: directory: $dir |"
-    echo "$TFLINT_CONFIG" > .tflint_config
-    docker run --rm  --workdir /data -v $(pwd):/data wata727/tflint:${TFLINT_VERSION} --config .tflint_config $dir || { exit_status=$?; printf "tflint check has failed. \n"; }
-    rm .tflint_config
-    # echo "exit status in TFLINT if $exit_status"
+  echo  "| TFLINT CHECK: directory: $dir |"
+  echo "$TFLINT_CONFIG" > .tflint_config
+  docker run --rm  --workdir /data -v $(pwd):/data wata727/tflint:${TFLINT_VERSION} --config .tflint_config $dir || { exit_status=$?; printf "tflint check has failed. \n"; }
+  rm .tflint_config
+  # echo "exit status in TFLINT if $exit_status"
     
-    echo  "| FORMATTING CHECK: directory: $dir |"
-    docker run --rm  --entrypoint terraform -v $(pwd):/apps alpine/terragrunt:${TF_VERSION} fmt --check /apps/$dir || { exit_status=$?;  printf "formating check has failed. \n"; }
-    # echo "exit status in FORMATTING if $exit_status"
-  fi
+  echo  "| FORMATTING CHECK: directory: $dir |"
+  docker run --rm  --entrypoint terraform -v $(pwd):/apps alpine/terragrunt:${TF_VERSION} fmt --check /apps/$dir || { exit_status=$?;  printf "formating check has failed. \n"; }
+  # echo "exit status in FORMATTING if $exit_status"
   # echo "exit status in loop $?"s
 done
 
 # check ./environments dir
-docker run --rm --entrypoint terragrunt -w /apps/environments -v $(pwd):/apps \
+docker run --rm --entrypoint terragrunt -w /apps/environment -v $(pwd):/apps \
 alpine/terragrunt:${TF_VERSION} hclfmt --terragrunt-check || { exit_status=$?;  printf "formating check has failed. \n"; }
 
 docker run --rm  --entrypoint terraform -w /apps -v $(pwd):/apps \
