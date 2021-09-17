72157bffeec1cbc7793e2deadf15a378557bd650
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-09-17 11:44:59 +0800

    Create efs.tf

---
+++ terraform_efs/efs.tf
@@ -0,0 +1,17 @@
+resource "aws_efs_file_system" "efs" {
+   creation_token = "efs"
+   performance_mode = "generalPurpose"
+   throughput_mode = "bursting"
+   encrypted = "true"
+ tags = {
+     Name = "EFS"
+   }
+ }
+
+
+resource "aws_efs_mount_target" "efs-mt" {
+   count = length(data.aws_availability_zones.available.names)
+   file_system_id  = aws_efs_file_system.efs.id
+   subnet_id = aws_subnet.subnet[count.index].id
+   security_groups = [aws_security_group.efs.id]
+ }
