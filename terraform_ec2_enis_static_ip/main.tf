7cc6746dd14c7fd7d3da93e410ba3477af4d05b8
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-10-06 16:54:33 +0800

    Update main.tf

--- terraform_ec2_enis_static_ip/main.tf
+++ terraform_ec2_enis_static_ip/main.tf
@@ -3,17 +3,17 @@ data "template_file" "userdata_script" {
 }
 
 
-resource "aws_instance" "appian_ec2_node1" {
+resource "aws_instance" "ec2_node1" {
   ami                  = "ami-0b0af3577fe5e3532"
   instance_type        = "m4.large"
   key_name             = "vault"
   user_data            = data.template_file.userdata_script.rendered
   network_interface {
-    network_interface_id = aws_network_interface.appian_inf_ec2_primary_1.id
+    network_interface_id = aws_network_interface.ec2_primary_1.id
     device_index         = 0
   }
   network_interface {
-    network_interface_id = aws_network_interface.appian_inf_ec2_mgmt_1.id
+    network_interface_id = aws_network_interface.ec2_mgmt_1.id
     device_index         = 1
   }
   root_block_device {
@@ -24,12 +24,12 @@ resource "aws_instance" "appian_ec2_node1" {
 }
 
 #-- create network interface for the EC2 instance in first AZ
-resource "aws_network_interface" "appian_inf_ec2_primary_1" {
+resource "aws_network_interface" "ec2_primary_1" {
   subnet_id       = "subnet-1e21093f"
   private_ips     = ["172.31.80.17"]
 }
 
-resource "aws_network_interface" "appian_inf_ec2_mgmt_1" {
+resource "aws_network_interface" "ec2_mgmt_1" {
   subnet_id       = "subnet-1e21093f"
   private_ips     = ["172.31.80.18"]
 }
