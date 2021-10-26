65cdab7f1d78d8549044aa73092ea925a955d441
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-10-26 16:42:22 +0800

    Update patching_completion_status.py

--- patching_workflow/patching_completion_status.py
+++ patching_workflow/patching_completion_status.py
@@ -10,11 +10,14 @@ def lambda_handler(event, context):
     for instance in instance_list:
         response = ec2_client.describe_instances(InstanceIds=[instance])
         instance_name= [tag['Value'] for tag in response['Reservations'][0]['Instances'][0]['Tags'] if tag['Key'] == 'Name']
-        response = ssm_client.list_compliance_items(ResourceIds=[instance],ResourceTypes=['ManagedInstance'])
-        if response['ComplianceItems'][0]['Status'] == 'COMPLIANT':
-            message.append(instance_name[0]+"("+instance+") patched successfully.\n ")
-        else:
+        #response = ssm_client.list_compliance_items(ResourceIds=[instance],ResourceTypes=['ManagedInstance'])
+        #print(response['ComplianceItems'][0]['Status'])
+        response=ssm_client.describe_instance_patch_states(InstanceIds=[instance])
+        #if response['ComplianceItems'][0]['Status'] == 'COMPLIANT':
+        if response['InstancePatchStates'][0]['MissingCount'] > 0:
             message.append(instance_name[0]+"("+instance+") not patched.\n ")
+        else:
+            message.append(instance_name[0]+"("+instance+") patched.\n ")
         
     
     sns_client.publish(TopicArn=os.environ['SNS_TOPIC'], Message=''.join(message) ,Subject="Patching Status")
