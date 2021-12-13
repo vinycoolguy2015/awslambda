f2dcb7d615151bf7acdf5e7083d33bb1d06ee491
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-12-13 23:34:41 +0800

    Update variables.tf

--- terraform_aws_backup/backup_module/variables.tf
+++ terraform_aws_backup/backup_module/variables.tf
@@ -1,45 +1,5 @@
-variable "backup_vault_name" {
-  type    = string
-  default = "example-vault"
-}
-
-variable "retention" {
-  type    = number
-  default = 7
-}
-
-variable "schedule" {
-  type    = string
-  default = "cron(15 7 ? * MON-FRI *)"
-}
-
-variable "start_window" {
-  type    = number
-  default = 60
-}
-
-variable "completion_window" {
-  type    = number
-  default = 360
-}
-
-variable "name" {
-  type    = string
-  default = "example-backup"
-}
-
-variable "key" {
-  type    = string
-  default = "example"
-}
-
-variable "value" {
-  type    = string
-  default = "example"
-}
-
-variable "resources_arn" {
-  type    = list(string)
-  default = []
-}
-
+variable "key" {}
+variable "sns_topic_arn" {}
+variable "value" {}
+variable "rules" {}
+variable "backup_vault_name" {}
