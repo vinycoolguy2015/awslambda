fb51d42a6ec7f307cdd2949197d08e75f45ea6ac
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-12-13 23:37:06 +0800

    Update main.tf

--- terraform_aws_backup/main.tf
+++ terraform_aws_backup/main.tf
@@ -1,10 +1,8 @@
-data "aws_db_instance" "database" {
-  db_instance_identifier = "database-2"
-}
-
 module "backup" {
-  source        = "./backup_module"
-  key           = var.key
-  value         = var.value
-  resources_arn = [data.aws_db_instance.database.db_instance_arn]
+  source            = "./backup_module"
+  key               = var.key
+  value             = var.value
+  backup_vault_name = var.backup_vault_name
+  rules             = var.rules
+  sns_topic_arn     = var.sns_topic_arn
 }
