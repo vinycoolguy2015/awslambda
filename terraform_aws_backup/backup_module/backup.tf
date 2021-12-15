f93998e1502e44cb8ccc271d835b54ff875fb8db
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-12-16 02:29:09 +0800

    Update backup.tf

--- terraform_aws_backup/backup_module/backup.tf
+++ terraform_aws_backup/backup_module/backup.tf
@@ -40,7 +40,7 @@ resource "aws_backup_plan" "backup-plan" {
   }
 }
 
-resource "aws_backup_selection" "example-server-backup-selection" {
+resource "aws_backup_selection" "backup-selection" {
   iam_role_arn = aws_iam_role.aws-backup-service-role.arn
   name         = "backup_resourcesi"
   plan_id      = aws_backup_plan.backup-plan.id
