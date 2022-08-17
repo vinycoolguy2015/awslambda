88817d73fc6cf7db742c1871c6c60756567570cc
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2022-08-17 14:08:16 +0800

    Update iam.tf

--- clamav_ecs/iam.tf
+++ clamav_ecs/iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "internet_ecs_task_execution_iam_role" {
+resource "aws_iam_role" "clamav_ecs_task_execution_iam_role" {
   name  = "iam-role-clamav-ecs-task-exe"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -50,5 +50,5 @@ Resource = "*"
 
 resource "aws_iam_role_policy_attachment" "aws-ecs-task-exe-policy" {
   policy_arn = aws_iam_policy.aws-ecs-task-exe-policy.arn
-  role       = aws_iam_role.internet_ecs_task_execution_iam_role.name
+  role       = aws_iam_role.clamav_ecs_task_execution_iam_role.name
 }
