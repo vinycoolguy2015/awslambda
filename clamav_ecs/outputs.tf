5ed7807a3ae1df5a1ad3ca59e574f27ea4dedc84
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2022-08-17 11:25:24 +0800

    Create outputs.tf

---
+++ clamav_ecs/outputs.tf
@@ -0,0 +1,7 @@
+output "clamav_lb_dns" {
+  value = aws_lb.test_aws_alb.dns_name
+}
+
+output "how_to_use" {
+  value = "curl -XPOST ${aws_lb.test_aws_alb.dns_name}/api/v1/scan -F FILES=@terraform.tfvars"
+}
