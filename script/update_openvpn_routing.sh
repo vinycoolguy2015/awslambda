8b3358ca4fad81f936b853d66bd474627d870ea0
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2022-09-20 16:18:27 +0800

    Update update_openvpn_routing.sh

--- script/update_openvpn_routing.sh
+++ script/update_openvpn_routing.sh
@@ -7,7 +7,8 @@ if test -f "$FILE"; then
     previous_ip_address=`cat $FILE`
     current_ip_address=`cat /tmp/current_ip.txt`
     if [ ! -z "$previous_ip_address" ] && [ ! -z "$current_ip_address" ];then
-        if [ "$current_ip_address" == "$previous_ip_address" ];then
+        diff $FILE /tmp/current_ip.txt
+        if [ "$?" == 0 ];then
             echo "IP Not Changed"
         else
             for ip in $previous_ip_address
@@ -21,6 +22,7 @@ if test -f "$FILE"; then
                     /usr/local/openvpn_as/scripts/sacli --key "vpn.server.routing.private_network.$current_rule_count" --value "$ip/32" ConfigPut
                 done
             /usr/local/openvpn_as/scripts/sacli start
+            cp /tmp/current_ip.txt $FILE
         fi
     fi
 fi
