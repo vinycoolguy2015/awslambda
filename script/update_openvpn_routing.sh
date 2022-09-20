2cb89bfe79d64876f8bee0fc663b689f8bbeadca
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2022-09-20 15:56:37 +0800

    Create update_openvpn_routing.sh

---
+++ script/update_openvpn_routing.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+  
+FILE=/home/openvpnas/dont_delete_ip.txt #Store current IP address in this file
+dig +short test.com | tail -n2 >/tmp/current_ip.txt
+
+if test -f "$FILE"; then
+    previous_ip_address=`cat $FILE`
+    current_ip_address=`cat /tmp/current_ip.txt`
+    if [ ! -z "$previous_ip_address" ] && [ ! -z "$current_ip_address" ];then
+        if [ "$current_ip_address" == "$previous_ip_address" ];then
+            echo "IP Not Changed"
+        else
+            for ip in $previous_ip_address
+                do
+                    rule_number=/usr/local/openvpn_as/scripts/sacli  ConfigQuery | grep $ip/32 | cut -d "\"" -f2 | cut -d "." -f5
+                    /usr/local/openvpn_as/scripts/sacli --key "vpn.server.routing.private_network.$rule_number" --value "$ip/32" ConfigDel
+                done
+            for ip in $current_ip_address
+                do
+                    current_rule_count=`/usr/local/openvpn_as/scripts/ConfigQuery | grep vpn.server.routing.private_network | wc -l`
+                    /usr/local/openvpn_as/scripts/sacli --key "vpn.server.routing.private_network.$current_rule_count" --value "$ip/32" ConfigPut
+                done
+            /usr/local/openvpn_as/scripts/sacli start
+        fi
+    fi
+fi
