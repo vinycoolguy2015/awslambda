895c557345bad49a17c1d5541221e4afe60b84db
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2023-03-19 17:49:03 +0800

    Update bamboo_deployment_status.sh

--- script/bamboo_deployment_status.sh
+++ script/bamboo_deployment_status.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 deployment_environment="DEV"
+current_time=`date +%s`
     
 project_id=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer NJjBrN58" --request GET --url https://bamboo.com/rest/api/latest/deploy/project/forPlan?planKey=TEST |jq '.[0].id'`
     
@@ -7,6 +8,14 @@ project_id=`curl --silent --insecure --header "Content-Type: application/json" -
 environment_id=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer NJjBrN58" --request GET --url https://bamboo.com/rest/api/latest/deploy/project/$project_id |jq --arg environment $deployment_environment '.environments[] | select(.name == $environment).id'`
     
     
-deployment_status=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer NJjBrN58" --request GET --url https://bamboo.com/rest/api/latest/deploy/environment/$environment_id/results?max-results=1|jq '.results[0].deploymentState'`
+deployment_status=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer NJjBrN58" --request GET --url https://bamboo.com/rest/api/latest/deploy/environment/$environment_id/results?max-results=1|jq '.results[0]'`
     
-echo $deployment_status
+deployment_start_date=`echo $deployment_status|jq '.startedDate'`
+deployment_status=`echo $deployment_status|jq '.deploymentState'`
+
+while [ $deployment_status != "SUCCESS" ]
+do
+  deployment_status=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer NJjBrN58" --request GET --url https://bamboo.com/rest/api/latest/deploy/environment/$environment_id/results?max-results=1|jq '.results[0].deploymentState'`
+  echo $deployment_status
+  sleep 5
+done
