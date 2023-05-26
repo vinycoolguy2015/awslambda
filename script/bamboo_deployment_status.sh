704d127727c1d15aef0630951da01c637ee1408f
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2023-05-26 10:59:58 +0800

    Update bamboo_deployment_status.sh

--- script/bamboo_deployment_status.sh
+++ script/bamboo_deployment_status.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-deployment_environment="DEV"
-current_time=`date +%s`
-    
-project_id=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer NJjBrN58" --request GET --url https://bamboo.com/rest/api/latest/deploy/project/forPlan?planKey=TEST |jq '.[0].id'`
-    
+deployment_environment="SIT"
+current_time=`date +%s%3N`
 
-environment_id=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer NJjBrN58" --request GET --url https://bamboo.com/rest/api/latest/deploy/project/$project_id |jq --arg environment $deployment_environment '.environments[] | select(.name == $environment).id'`
-    
-    
-deployment_status=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer NJjBrN58" --request GET --url https://bamboo.com/rest/api/latest/deploy/environment/$environment_id/results?max-results=1|jq '.results[0]'`
-    
-deployment_start_date=`echo $deployment_status|jq '.startedDate'`
-deployment_status=`echo $deployment_status|jq '.deploymentState'|tr -d '"'`
+project_id=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer MDQ"--request GET --url https://bamboo.com/rest/api/latest/deploy/project/forPlan?planKey=TEST |jq '.[0].id'`
 
-while [ $deployment_status != "SUCCESS" ]
+
+environment_id=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer MDQ" --request GET --url https://bamboo.com/rest/api/latest/deploy/project/$project_id |jq --arg environment $deployment_environment '.environments[]| select(.name == $environment).id'`
+
+
+deployment=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer MDQ"--request GET --url https://bamboo.com/rest/api/latest/deploy/environment/$environment_id/results?max-results=1|jq '.results[0]'`
+
+deployment_start_date=`echo $deployment|jq '.startedDate'`
+deployment_status=`echo $deployment|jq '.deploymentState'|tr -d '"'`
+
+while [ "$deployment_start_date" -gt $current_time ]
 do
-  deployment_status=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer NJjBrN58" --request GET --url https://bamboo.com/rest/api/latest/deploy/environment/$environment_id/results?max-results=1|jq '.results[0].deploymentState'|tr -d '"'`
-  echo $deployment_status
+        echo "Waiting for Deployment to start"
+        deployment=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer MDQ" --request GET --url https://bamboo.com/rest/api/latest/deploy/environment/$environment_id/results?max-results=1|jq '.results[0]'`
+        deployment_start_date=`echo $deployment|jq '.startedDate'`
+        sleep 10
+done
+echo "Deployment started"
+while [ "$deployment_status" != "SUCCESS" ]
+do
+  deployment_status=`curl --silent --insecure --header "Content-Type: application/json" --header "Authorization: Bearer MDQ" --request GET --url https://bamboo.com/rest/api/latest/deploy/environment/$environment_id/results?max-results=1|jq '.results[0].deploymentState'|tr -d '"'`
+  echo "Deployment Status: $deployment_status"
   sleep 5
 done
+
+echo "Deployment Status: $deployment_status"
